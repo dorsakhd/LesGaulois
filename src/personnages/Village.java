5f06e3b5ad9b19cdd497dce460870715da8ba114
dorsakhd <dorsakhoshnoud@gmail.com>
2023-10-24 19:02:45 +0200

    TP2c

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,10 +2,33 @@ package personnages;
 
 public class Village {
 private String nom;
+private Chef chef;
+private Gaulois[] villageois;
+private int nbVillageois; 
+
+public Village(String nom, int nbVillageoisMaximum) {
+    this.nom = nom;
+    this.nbVillageois = 0;
+    this.villageois = new Gaulois[nbVillageoisMaximum];
+}
 public Village(String nom) {
 this.nom = nom;
+this.nbVillageois = 0;
+}
+public void setChef(Chef chef) {
+this.chef = chef;
 }
 public String getNom() {
 return nom;
 }
+public void ajouterHabitant(Gaulois gaulois) {
+    if (nbVillageois < villageois.length) {
+        villageois[nbVillageois] = gaulois;
+        nbVillageois++;
+        System.out.println(gaulois.getNom() + " a été ajouté au village de " + nom);
+    } else {
+        System.out.println(" village est full.");
+    }
+}
+
 }
